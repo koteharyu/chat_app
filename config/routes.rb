75bf77588173708be606b4162d28b616e6686332
haryu <hryau6@gmail.com>
2021-06-12 16:54:35 +0900

    add: routing of rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'rooms/show'
+  root to: 'homes#top'
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  root to: 'homes#top'
+  resources :rooms, only: [:show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
