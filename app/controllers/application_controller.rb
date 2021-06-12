6d13da1108df913e915f56bdddd4419140f4fe8c
haryu <hryau6@gmail.com>
2021-06-12 16:29:43 +0900

    add: before action to application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configurte_permitted_parameters, if: :devise_controller?
+
+  private
+  def configurte_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_un, key: [:email])
+  end
 end
