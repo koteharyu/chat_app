86bbd7b2e485eba157d63d196dc5d69f39f13034
haryu <hryau6@gmail.com>
2021-06-12 16:34:04 +0900

    fix: application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configurte_permitted_parameters, if: :devise_controller?
-
-  private
-  def configurte_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_un, key: [:email])
-  end
 end
