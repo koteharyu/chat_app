7414ebf5b75ce015fa5e7c68932906e915649e46
haryu <hryau6@gmail.com>
2021-06-12 16:57:03 +0900

    add: rooms/show and create actions

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,18 @@
 class RoomsController < ApplicationController
   def show
+    @room = Room.find(params[:id])
+  end
+
+  def create
+    @room = Room.new(room_params)
+    if @room.save
+      current_user.user_rooms.create(room_id: @room.id)
+      redirect_to @room
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name)
   end
 end
